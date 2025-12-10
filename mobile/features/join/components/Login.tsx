7277a9e729e2303e53f3f7482c113906f9719928
Jake Toomay <jake@toomay.dev>
2025-12-09 19:11:33 -0600

    Fix the keybard on the sign up and login page

--- mobile/features/join/components/Login.tsx
+++ mobile/features/join/components/Login.tsx
@@ -1,94 +1,105 @@
 import { SignInCardProps } from "@/app/join";
 import React, { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 export default function Login({ setSignUp }: SignInCardProps) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View
-      style={{
-        alignItems: "center",
-        marginTop: "60%",
-        width: "100%",
-        flex: 1,
-      }}
-    >
-      <Text style={{ fontSize: 40, fontWeight: 700, color: "#e0f2fe" }}>
-        Sign In
-      </Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View
         style={{
-          marginTop: 25,
+          alignItems: "center",
+          marginTop: "60%",
           width: "100%",
           flex: 1,
-          padding: 50,
-          gap: 50,
         }}
       >
-        <View style={{ gap: 15 }}>
-          <View style={{ gap: 10 }}>
-            <Text style={{ color: "#e0f2fe" }}>Email</Text>
-            <TextInput
-              style={{
-                width: "100%",
-                backgroundColor: "#e0f2fe",
-                height: 45,
-                borderRadius: 10,
-                padding: 10,
-                color: "black",
-              }}
-              value={email}
-              onChangeText={setEmail}
-              placeholder="Enter your email..."
-              placeholderTextColor="black"
-            />
-          </View>
-          <View style={{ gap: 10 }}>
-            <Text style={{ color: "#e0f2fe" }}>Password</Text>
-            <TextInput
-              style={{
-                width: "100%",
-                backgroundColor: "#e0f2fe",
-                height: 45,
-                borderRadius: 10,
-                padding: 10,
-                color: "black",
-              }}
-              secureTextEntry={true}
-              value={password}
-              onChangeText={setPassword}
-              placeholder="Enter your password..."
-              placeholderTextColor="black"
-            />
-          </View>
-        </View>
+        <Text style={{ fontSize: 40, fontWeight: 700, color: "#e0f2fe" }}>
+          Sign In
+        </Text>
         <View
           style={{
-            gap: 10,
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: "auto",
+            marginTop: 25,
+            width: "100%",
+            flex: 1,
+            padding: 50,
+            gap: 50,
           }}
         >
-          <TouchableOpacity
+          <View style={{ gap: 15 }}>
+            <View style={{ gap: 10 }}>
+              <Text style={{ color: "#e0f2fe" }}>Email</Text>
+              <TextInput
+                style={{
+                  width: "100%",
+                  backgroundColor: "#e0f2fe",
+                  height: 45,
+                  borderRadius: 10,
+                  padding: 10,
+                  color: "black",
+                }}
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Enter your email..."
+                placeholderTextColor="black"
+              />
+            </View>
+            <View style={{ gap: 10 }}>
+              <Text style={{ color: "#e0f2fe" }}>Password</Text>
+              <TextInput
+                style={{
+                  width: "100%",
+                  backgroundColor: "#e0f2fe",
+                  height: 45,
+                  borderRadius: 10,
+                  padding: 10,
+                  color: "black",
+                }}
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Enter your password..."
+                placeholderTextColor="black"
+              />
+            </View>
+          </View>
+          <View
             style={{
-              backgroundColor: "#0ea5e9",
-              padding: 15,
-              borderRadius: 10,
+              gap: 10,
+              justifyContent: "center",
               alignItems: "center",
-              width: "100%",
+              marginTop: "auto",
             }}
           >
-            <Text style={{ fontWeight: 600, color: "#e0f2fe" }}>Continue</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setSignUp(true)}>
-            <Text
-              style={{ color: "#e0f2fe" }}
-            >{`Don’t have an account? Sign up`}</Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#0ea5e9",
+                padding: 15,
+                borderRadius: 10,
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Text style={{ fontWeight: 600, color: "#e0f2fe" }}>
+                Continue
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setSignUp(true)}>
+              <Text
+                style={{ color: "#e0f2fe" }}
+              >{`Don’t have an account? Sign up`}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
