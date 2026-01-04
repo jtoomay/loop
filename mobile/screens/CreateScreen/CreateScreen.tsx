eece29a25e1b0194da13dbdb8817893a30cc5ce1
Jake Toomay <jake@toomay.dev>
2026-01-03 19:32:54 -0600

    Date Time Picker

--- mobile/screens/CreateScreen/CreateScreen.tsx
+++ mobile/screens/CreateScreen/CreateScreen.tsx
@@ -1,11 +1,39 @@
-import { Screen } from '@/design/layout'
-import { P } from '@/design/text'
-import { memo } from 'react'
+import { Input } from '@/design/inputs'
+import { Screen, VStack } from '@/design/layout'
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker'
+import { memo, useEffect, useState } from 'react'
 
 const CreateScreen = memo(function CreateScreen() {
+  const [title, setTitle] = useState('')
+  const [time, setTime] = useState(new Date())
+
+  //TODO: Remove later once we make the function to send to db
+  useEffect(() => {
+    console.log(
+      'Time: ',
+      time.toLocaleTimeString(undefined, {
+        hour12: false,
+      }),
+    )
+  }, [time])
+
+  const onChange = (_: DateTimePickerEvent, selectedTime?: Date) => {
+    const currentTime = selectedTime || time
+    setTime(currentTime)
+  }
   return (
-    <Screen>
-      <P color='primary'>Create</P>
+    <Screen padding={4}>
+      <VStack gap={4}>
+        <Input value={title} onChangeText={setTitle} placeholder='Title...' />
+        <DateTimePicker
+          mode='time'
+          display='default'
+          value={time}
+          onChange={onChange}
+        />
+      </VStack>
     </Screen>
   )
 })
